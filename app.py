e53c33010e0bc2968b69c01d0733f0acb359311e
Rajesh <rajeshff220375@gmail.com>
2025-07-23 12:14:30 +0530

    commit changes 2

--- app.py
+++ app.py
@@ -4,4 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, Vercel!'
+    return "Hello from Flask on Vercel!"
+
+# Only required if you're testing locally
+if __name__ == '__main__':
+    app.run(debug=True)
+
